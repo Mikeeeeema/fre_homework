e7724d2d463d096d2b7f49b3b8b11fb6fcb2daa7
Xioamaaa <143615414+Xioamaaa@users.noreply.github.com>
2025-01-01 15:03:40 -0600

    hw8-1

--- hw8-albumSearchApp/src/app/app.module.ts
+++ hw8-albumSearchApp/src/app/app.module.ts
@@ -5,17 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { AlbumslistComponent } from './components/albumslist/albumslist.component';
-import { AlbumitemComponent } from './components/albumitem/albumitem.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchComponent,
-    AlbumslistComponent,
-    AlbumitemComponent,
-  ],
+  declarations: [AppComponent, SearchComponent, AlbumslistComponent],
   //formsmodule for the two way binding
   //HttpClientModule for send http request
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
